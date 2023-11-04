36dc98669db4b37c39a7b1146456c694e7480c67
haziqhalim <haziqhalim@localhost>
2023-11-04 10:34:37 +0800

    Added testingClass class file

---
+++ NetBeansProjects/PriceTracker/src/Login/testingClass.java
@@ -0,0 +1,13 @@
+package Login;
+
+public class testingClass
+{
+    
+    public static void main(String[] args)
+    {
+        
+        System.out.println("Hey hey hey! Trying to push this into the remote repository...");
+        
+    }
+    
+}
