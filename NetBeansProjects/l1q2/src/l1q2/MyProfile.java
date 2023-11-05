3c3877a0e4c436d2b70447fe34b818a02e5dace1
haziqhalim <haziqhalim@localhost>
2023-11-05 17:53:36 +0800

    Uploaded L1Q2

---
+++ NetBeansProjects/l1q2/src/l1q2/MyProfile.java
@@ -0,0 +1,17 @@
+package l1q2;
+
+public class MyProfile
+{
+    
+    public static void main(String[] args)
+    {
+        
+        System.out.println("Full Name: 'Ammar Haziq bin Abdul Halim");
+        System.out.println("Matrics Number: 22002548/2");
+        System.out.println("Address: 75, Jalan Anyjalan, Bandar Anybandar, 12345 Kuala Lumpur, Malaysia");
+        System.out.println("Email: hazhalim0507@gmail.com");
+        System.out.println("Contact Number: +6012-7273192");
+        
+    }
+    
+}
