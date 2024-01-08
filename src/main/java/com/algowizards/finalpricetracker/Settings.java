ac2690aaff7f38d7dc138daf73035e51d734ff9a
USER <USER@TengkuHaikal>
2024-01-08 22:21:17 +0800

    dah buat nak change username dgn password, tpppp..... cuba u run, i taktahu macam mana nak explain

--- src/main/java/com/algowizards/finalpricetracker/Settings.java
+++ src/main/java/com/algowizards/finalpricetracker/Settings.java
@@ -7,6 +7,7 @@ package com.algowizards.finalpricetracker;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.IOException;
 
 /**
  *
@@ -16,7 +17,7 @@ public class Settings {
 
     static Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("\n-----= PrizeWizard Account Settings =-----\n");
         System.out.println("Hello, " + UserManager.getCurrentUser().getFirstName() + "!");
         System.out.println("""
@@ -25,45 +26,74 @@ public class Settings {
                            """);
         System.out.print("Enter your choice (1/2) --> ");
         int userChoice = keyboard.nextInt();
+        System.out.println("");
 
         switch (userChoice) {
+            
             case 1: {
-                int loop = 0; // to loop if current username is not matched
+                
+                //int loop = 0; // to loop if current username is not matched
 
-                do {
-                    System.out.println("Answer this question before changing your username.");
-                    System.out.print(UserManager.getCurrentUser().getSecurityQuestion());
-                    System.out.print("Your answer : ");
-                    String securityAnswer = keyboard.nextLine();
+                System.out.println("Answer this question before changing your username.");
+                System.out.println(UserManager.getCurrentUser().getSecurityQuestion());
+                System.out.print("Your answer : ");
+                String securityAnswer = keyboard.nextLine();
 
-                    UserManager.readFromFile(); // Load the list of users into the program
+                UserManager.readFromFile(); // Load the list of users into the program
 
-                    if (UserManager.getUserList() != null) // AKA if the file is not empty
-                    {
+                if (UserManager.getUserList() != null){ // AKA if the file is not empty
 
-                        if (!UserManager.getCurrentUser().getSecurityAnswer().equals(securityAnswer)) {
+                    if (!UserManager.getCurrentUser().getSecurityAnswer().equals(securityAnswer)) {
 
-                            System.out.println("The security answer you enter is not match to your current username.\n");
-                            loop = 1; //to loop again the process until user enter the correct username
-                            //return;
+                        System.out.println("The security answer you enter is not match to your current username.\n");
+                        //loop = 1; //to loop again the process until user enter the correct username
+                        //return;
 
-                        } else {
+                    } else {
 
-                            System.out.println("Alright! Now, set your new username : ");
-                            String newUsername = keyboard.nextLine();
-                            UserManager.getCurrentUser().setUsername(newUsername);
-                            List<String[]> intermediateUser = new ArrayList<>();
-                            for (User user : UserManager.getUserList()){
-                                intermediateUser.add(UserManager.toArray(user));
-                            }
-                            FileManager.write2DListIntoCSVFile()
-                            UserManager.writeToFile();
-                        }
+                        System.out.println("Alright! Now, set your new username : ");
+                        String newUsername = keyboard.nextLine();
 
+                        UserManager.getCurrentUser().setUsername(newUsername);
+
+                        UserManager.writeToUserInformationFile();
                     }
-
-                } while (loop == 1);
+                }
+                
+                break;
+                
             }
+            case 2 : {
+                
+                System.out.println("Answer this question before changing your password.");
+                System.out.println(UserManager.getCurrentUser().getSecurityQuestion());
+                System.out.print("Your answer : ");
+                String securityAnswer = keyboard.nextLine();
+
+                UserManager.readFromFile(); // Load the list of users into the program
+
+                if (UserManager.getUserList() != null){ // AKA if the file is not empty
+
+                    if (!UserManager.getCurrentUser().getSecurityAnswer().equals(securityAnswer)) {
+
+                        System.out.println("The security answer you enter is not match to your current username.\n");
+                        //loop = 1; //to loop again the process until user enter the correct username
+                        //return;
+
+                    } else {
+
+                        System.out.println("Alright! Now, set your new password : ");
+                        String newPassword = keyboard.nextLine();
+
+                        UserManager.getCurrentUser().setPassword(newPassword);
+
+                        UserManager.writeToUserInformationFile();
+                    }
+                }
+            }
+            
+            break;
+            
         }
     }
 }
